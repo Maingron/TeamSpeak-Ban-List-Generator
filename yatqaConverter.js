8a1c6fd5c468bdc68a8e80c51f08dd3a92e0e1d6
Maingron <r@maingron.com>
2024-10-20 01:28:29 +0200

    Add yatquaConverter.convertReason()

--- yatqaConverter.js
+++ yatqaConverter.js
@@ -1,6 +1,13 @@
 var yatqaConverter = {
 	handleOneWordEntry: function(inputEntry) {
-		return "name=" + inputEntry[0] + " duration=0 banreason=" + inputEntry[1] + " \r\n";
+		return "name=" + inputEntry[0] + " duration=0 banreason=" + this.convertReason(inputEntry[1]) + " \r\n";
+	},
+
+	convertReason: function(inputReason) {
+		let reason = inputReason;
+		reason = reason.replaceAll(" ", "\\s");
+
+		return reason;
 	},
 
 	handleWordCollection: function(inputCollection) {
@@ -11,6 +18,5 @@ var yatqaConverter = {
 		}
 
 		return processedWordCollection;
-
 	}
 }
