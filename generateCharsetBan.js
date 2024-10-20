10cb52822045f9ed0d1a0989fce63ac90a5e1672
Maingron <r@maingron.com>
2024-10-20 03:22:00 +0200

    Improve on collections

--- generateCharsetBan.js
+++ generateCharsetBan.js
@@ -1,16 +1,25 @@
 var charsetCollection = [
 	// [["🅰️", "🅱️", "🅾️", "🆎"], reasons.characters.bloodType],
 	// [["[🅰️🅱️🅾️🆎]"], reasons.characters.bloodType],
-	[[".?.?.?"],reasons.characters.consSpace],
+	[["\\s\\s\\s"], reasons.characters.consSpace],
+	[["[ƒƑ]"], reasons.characters.fakeF],
+	[["[ց]"], reasons.characters.fakeG],
+	[["[ľŀ]"], reasons.characters.fakeL],
 	[["[ಽട]"], reasons.characters.fakeS],
+	[["[–—]"], reasons.characters.fakeDash],
+	[["[ƦɌѓЃґҐӷӶԇԆ]"], reasons.characters.fakeR],
 	[["[️️️]"], reasons.characters.tsbug1],
 	[["[␀␁␂␃␄␅␆␇␈␉␊␋␌␍␎␏␐␑␒␓␔␕␖␗␘␙␚␛␜␝␞␟␠␡]"], reasons.characters.control],
+	[["[🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶🇷🇸🇹🇺🇻🇼🇽🇾🇿]"], reasons.characters.regionalIndicatorLetters],
+	[["[🖕]"], reasons.characters.middleFinger],
+	[["[⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵]"], reasons.characters.enclosedAlphanumerics2],
+	[["[ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ]"], reasons.characters.enclosedAlphanumerics],
 	[["Ⓜ️"], reasons.test]
 ];
 
 // Invalid, and thus already "banned" charsets:
-// [["[𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ]"], reasons.characters.mathDoubleStruckUpper],
-// [["[𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫]"], reasons.characters.mathDoubleStruckLower],
+// [["[𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤℾℿ]"], reasons.characters.mathDoubleStruckUpper],
+// [["[𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫ℼℽ]"], reasons.characters.mathDoubleStruckLower],
 
 function generateBanCharsetOne(inputEntry) {
 	let processedEntry = [];
