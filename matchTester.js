5a443ced376211688dba7b2447f388dc6313b868
Maingron <r@maingron.com>
2025-02-17 15:29:09 +0000

    Add match tester

---
+++ matchTester.js
@@ -0,0 +1,19 @@
+function matchTester(input, against) {
+	let matchArray = [];
+	for(let compareTo of against) {
+		if(input.match(compareTo[0])) {
+			matchArray.push(compareTo);
+		}
+	}
+
+	matchTesterToOutput(matchArray);
+
+	return matchArray;
+}
+
+function matchTesterToOutput(matchArray, textareaElement = document.querySelector(".match-tester--matches")) {
+	textareaElement.value = "";
+	for(let match of matchArray) {
+		textareaElement.value += match[0] + "\t\t" + match[1] + "\n";
+	}
+}
